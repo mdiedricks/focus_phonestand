9a881e3dd076716873428a0ac6871811a369e26f
mdiedricks <michael.diedricks@gmail.com>
2021-07-07 16:13:06 +1000

    setup initial button press

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Focus Phonestand
 
 A prototype for a phonestand that will inform your when you have not picked up your phone in the last 30minutes - or however long it you configure it for.
+
+# Focus Phonestand
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
 
+#define PINOUT 8
+#define PININ 9
+
+int longPress = 500;
+long timeDown = 0;
+bool buttonPressed = false;
+
 void setup() {
-  // put your setup code here, to run once:
-}
+  Serial.begin(9600);
+  pinMode(PINOUT, OUTPUT);
+  pinMode(PININ, INPUT);
+
+  digitalWrite(PINOUT, HIGH);
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  while(digitalRead(PININ) == HIGH){
+    delay(100);
+    timeDown = timeDown + 100;
+  } 
+
+  if(timeDown >= longPress){
+    timeDown = 0;
+    Serial.print("Button has been pressed");
+  }
+
+};
