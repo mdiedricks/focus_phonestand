22febce434419cdca4827ae12327ff2459c7239b
mdiedricks <michael.diedricks@gmail.com>
2021-07-08 13:55:04 +1000

    reconfigured button setup with pulldown on circuit

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,33 @@
 #include <Arduino.h>
 
-#define PINOUT 8
 #define PININ 9
 
 int longPress = 500;
+
 long timeDown = 0;
 bool buttonPressed = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PINOUT, OUTPUT);
   pinMode(PININ, INPUT);
 
-  digitalWrite(PINOUT, HIGH);
+  Serial.println("Ready to go!");
 };
 
 void loop() {
-  while(digitalRead(PININ) == HIGH){
+  while (digitalRead(PININ) == HIGH) {
     delay(100);
     timeDown = timeDown + 100;
-  } 
+  }
 
-  if(timeDown >= longPress){
-    timeDown = 0;
-    Serial.print("Button has been pressed");
+  if (timeDown >= longPress) {
+      timeDown = 0;
+      buttonPressed = true;
+    }
+
+  if (buttonPressed == true) {
+    Serial.println("===Button pressed");
+    buttonPressed = false;
   }
 
 };
