bec3006dfe4e6e94d5a57b66348fcb7bbf4bb96b
mdiedricks <michael.diedricks@gmail.com>
2021-07-08 17:32:48 +1000

    built out skeleton logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,74 @@
+#include <RTClib.h>
 #include <Arduino.h>
 
-#define PININ 9
+#define PHONEPIN 9
+#define TRIGGER 10
 
-int longPress = 500;
+RTC_DS3231 rtc;
 
-long timeDown = 0;
-bool buttonPressed = false;
+bool inSession = false;
+bool phoneDown = false;
+int timeStart_s;
+int timeStart_m;
+int timeDown_s = 0;
+int timeDown_m = 0;
+int focusDuration = 25;
+DateTime timeTrack;
+TimeSpan timeEnd;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PININ, INPUT);
+  Serial.println("Setting up");
+  pinMode(PHONEPIN, INPUT);
 
-  Serial.println("Ready to go!");
 };
 
 void loop() {
-  while (digitalRead(PININ) == HIGH) {
-    delay(100);
-    timeDown = timeDown + 100;
-  }
-
-  if (timeDown >= longPress) {
-      timeDown = 0;
-      buttonPressed = true;
+  //UPDATE TIMER
+  if (inSession) {
+    timeTrack = rtc.now();
+    if (timeDown_m < 1) {
+      if (timeDown_s != timeTrack.second()) {
+        timeDown_s = timeTrack.second();
+      }
+    }
+    if (timeDown_m != timeTrack.minute()) {
+      timeDown_m = timeTrack.minute();
     }
 
-  if (buttonPressed == true) {
-    Serial.println("===Button pressed");
-    buttonPressed = false;
   }
 
+  //CHECK SWITCH
+  if (digitalRead(PHONEPIN) == LOW) {
+    phoneDown = false;
+  } else {
+    phoneDown = true;
+  }
+
+  // POINT OF NO RETURN
+  if (phoneDown && !inSession) {
+    if (timeDown_s > TRIGGER) {
+      DateTime now = rtc.now();
+      timeStart_s = now.second();
+      timeStart_m = now.second();
+      timeEnd = TimeSpan(0, 0, focusDuration, 0);
+      inSession = true;
+    }
+  }
+
+  // LIFT PHONE
+  while (!phoneDown && inSession) {
+    if (timeDown_m < focusDuration) {
+      //make noise
+    } else {
+      reset();
+      break;
+    }
+  }
 };
+
+void reset() {
+  inSession = false;
+  timeDown_m = 0;
+  timeDown_s = 0;
+}
